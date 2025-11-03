2d2e42c9db9a0bd682fdb9037d8a487534c0cd86
Armin Daniel <antonio9949257@gmail.com>
2025-11-03 19:51:05 -0400

    lab 6 fucionando

--- Controllers/CursoController.cs
+++ Controllers/CursoController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using MiIT.Models;
 
 namespace MiIT.Controllers
 {
@@ -7,6 +8,20 @@ namespace MiIT.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult CursosJson()
+        {
+            var cursos = new List<Curso>
+            {
+                new Curso { idCurso = 1, nombre = "CAI-500 CONTROL Y AUTOMATIZACIÓN INDUSTRIAL II", descripcion = "Descripción de CAI-500", bhabilitado = true },
+                new Curso { idCurso = 2, nombre = "TEW-500 TECNOLOGÍA WEB II", descripcion = "Descripción de TEW-500", bhabilitado = true },
+                new Curso { idCurso = 3, nombre = "SII-500 SISTEMAS DE INFORMACIÓN II", descripcion = "Descripción de SII-500", bhabilitado = true },
+                new Curso { idCurso = 4, nombre = "TEL-500 TELEMÁTICA II", descripcion = "Descripción de TEL-500", bhabilitado = true },
+                new Curso { idCurso = 5, nombre = "EMP-500 EMPRENDIMIENTO PRODUCTIVO I", descripcion = "Descripción de EMP-500", bhabilitado = true },
+                new Curso { idCurso = 6, nombre = "TMG-500 TALLER DE MODALIDAD DE GRADUACIÓN I", descripcion = "Descripción de TMG-500", bhabilitado = true }
+            };
+            return Json(cursos);
         }
     }
 }
---
+++ Models/Curso.cs
@@ -0,0 +1,10 @@
+namespace MiIT.Models
+{
+    public class Curso
+    {
+        public int idCurso { get; set; }
+        public string? nombre { get; set; }
+        public string? descripcion { get; set; }
+        public bool bhabilitado { get; set; }
+    }
+}
